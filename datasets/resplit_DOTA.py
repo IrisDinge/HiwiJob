facbe5c9adb3018350dc949981bc575162e2e98b
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2020-01-14 17:29:37 +0100

    Update resplit_DOTA.py

--- datasets/resplit_DOTA.py
+++ datasets/resplit_DOTA.py
@@ -16,11 +16,18 @@ def merge_data(root, training_path, validation_path):
     all.to_csv('/home/dingjin/HiwiJob/example/all.csv', sep=' ', header=None, index=False)
 
 
+
+def split_dataset(all_path):
+    data = pd.read_csv(all_path)
+    x, y = data.iloc[:, 1:], data.iloc[:, 0]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+
+
+
+
 if __name__ == '__main__':
     merge_data('/home/dingjin/HiwiJob/example/',
           '/home/dingjin/HiwiJob/example/train/list.csv',
           '/home/dingjin/HiwiJob/example/validation/list.csv')
-    data = pd.read_csv('/home/dingjin/HiwiJob/example/all.csv')
-    x, y = data.iloc[:, 1:], data.iloc[:, 0]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
-    print(x_train)
+    split_dataset('/home/dingjin/HiwiJob/example/all.csv')
