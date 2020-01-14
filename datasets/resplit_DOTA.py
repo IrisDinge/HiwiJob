a64c0daf0838cda44e472895ea1af67af1966d25
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2020-01-14 17:17:35 +0100

    Update and rename split.py to resplit_DOTA.py

---
+++ datasets/resplit_DOTA.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+"""
+***Main Function***
+1. merge training set and validation set, then output all.csv: <image_path class-id>
+2. split data set based on .csv, train and test proportion defined by parameter:test_size
+
+
+
+"""
+def merge_data(root, training_path, validation_path):
+    training_data = pd.read_csv(training_path, header=None, sep=' ')
+    validation_data = pd.read_csv(validation_path, header=None, sep=' ')
+    all = training_data.append(validation_data)
+    all.to_csv('/home/dingjin/HiwiJob/example/all.csv', sep=' ', header=None, index=False)
+
+
+if __name__ == '__main__':
+    merge_data('/home/dingjin/HiwiJob/example/',
+          '/home/dingjin/HiwiJob/example/train/list.csv',
+          '/home/dingjin/HiwiJob/example/validation/list.csv')
+    data = pd.read_csv('/home/dingjin/HiwiJob/example/all.csv')
+    x, y = data.iloc[:, 1:], data.iloc[:, 0]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+    print(x_train)
