2b329bb9778ac9db5219bc06e480c2fdc255fc42
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2020-01-15 15:52:55 +0100

    Update entrance.py

--- entrance.py
+++ entrance.py
@@ -1,40 +1,57 @@
-import numpy as np
 from six.moves import cPickle as pickle
-import os
+import csv
 import sys
-['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 'small-vehicle', 'large-vehicle', 'ship',
-              'tennis-court', 'basketball-court', 'storage-tank', 'soccer-ball-field', 'roundabout', 'harbor',
-              'swimming-pool', 'helicopter']
+sys.path.insert(0, '.')
 
-DEBUG = True
-ROOT = '/data/vehicle-classification/dota/tmp'
+"""
+***Main Function***
+1. get train/test files
+2. parse train/test files
+3. generate train/test files 
+"""
 
-TRAINSET = [
-    ('train', 'plane'),
-    ('train', 'baseball-diamond'),
-    ('train', 'bridge'),
-    ('train', 'ground-track-field'),
-    ('train', 'small-vehicle'),
-    ('train', 'large-vehicle'),
-    ('train', 'ship'),
-    ('train', 'tennis-court'),
-    ('train', 'basketball-court'),
-    ('train', 'storage-tank'),
-    ('train', 'soccer-ball-field'),
-    ('train', 'roundabout'),
-    ('train', 'harbor'),
-    ('train', 'swimming-pool'),
-    ('train', 'helicopter'),
-    ]
+ROOT = '/home/dingjin/HiwiJob/example/'
+txt_file = ['train', 'test']
 
-TESTSET = [
-    ('test'),
-    ]
 
-if __name__=='__main__':
-    print('Getting training files')
-    train= []
-    for (img_set, class_id) in TRAINSET:
-        with open(f'{ROOT}/{img_set}/list.csv', 'r') as f:
-            ids = f.read().strip().split()
-        train += [f'{ROOT}/']
+#def search_image():
+#    root_dir = ROOT
+#    return f'{root_dir}/{csv_file}/'
+
+
+
+
+if __name__ == '__main__':
+    print("Getting Dataset")
+    train = []
+    with open(f'{ROOT}/train.txt', 'r') as f:
+        image_path = f.readlines()
+        print(image_path)
+
+
+
+
+
+
+
+
+
+"""
+def pickle_from_csv(root):
+
+    for i in range(len(csv_file)):
+        csv_path = root + csv_file[i] + '.csv'
+        pkl_path = root + csv_file[i] + '.csv'
+        print(csv_path, pkl_path)
+        x = []
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            for line in reader:x.append(line)
+        with open(pkl_path, 'wb') as f:
+            pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
+
+
+
+pickle_from_csv('/home/dingjin/HiwiJob/example/')
+
+"""
