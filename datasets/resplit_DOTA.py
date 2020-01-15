6d56c38be02f7b88dfde768ab0a8af28393f2f36
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2020-01-15 13:09:46 +0100

    Update resplit_DOTA.py

--- datasets/resplit_DOTA.py
+++ datasets/resplit_DOTA.py
@@ -5,29 +5,32 @@ from sklearn.model_selection import train_test_split
 ***Main Function***
 1. merge training set and validation set, then output all.csv: <image_path class-id>
 2. split data set based on .csv, train and test proportion defined by parameter:test_size
+3. output: 2 .csv files: train.csv and test.csv
+    <image_path class-id >
 
 
 
 """
-def merge_data(root, training_path, validation_path):
+def merge_data(root):
+    training_path = root + 'train/' + 'list.csv'
+    validation_path = root + 'validation/' + 'list.csv'
     training_data = pd.read_csv(training_path, header=None, sep=' ')
     validation_data = pd.read_csv(validation_path, header=None, sep=' ')
-    all = training_data.append(validation_data)
-    all.to_csv('/home/dingjin/HiwiJob/example/all.csv', sep=' ', header=None, index=False)
+    all = training_data.append(validation_data, ignore_index=True)
+    all.to_csv(root+'all.csv', sep=' ', header=None, index=False)
 
 
 
-def split_dataset(all_path):
-    data = pd.read_csv(all_path)
-    x, y = data.iloc[:, 1:], data.iloc[:, 0]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
-
-
+def split_dataset(root):
+    merge_data(root)
+    data = pd.read_csv(root + 'all.csv', header=None, sep=' ')
+    x = data.iloc[:]
+    x_train, x_test = train_test_split(x, test_size=0.3, random_state=0, )
+    x_train.to_csv('/home/dingjin/HiwiJob/example/train.csv', sep=' ', header=None, index=False)
+    x_test.to_csv('/home/dingjin/HiwiJob/example/test.csv', sep=' ', header=None, index=False)
 
 
 
 if __name__ == '__main__':
-    merge_data('/home/dingjin/HiwiJob/example/',
-          '/home/dingjin/HiwiJob/example/train/list.csv',
-          '/home/dingjin/HiwiJob/example/validation/list.csv')
-    split_dataset('/home/dingjin/HiwiJob/example/all.csv')
+
+    split_dataset('/home/dingjin/HiwiJob/example/')
